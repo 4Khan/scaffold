1a44f5fa4a76a1bc415d75bf8e5bd3a819159ddb
4khan <amaan_khan@live.co.uk>
2024-06-25 09:49:57 +0000

    adding initial structure

---
+++ hello.py
@@ -0,0 +1,10 @@
+def add(x, y):
+    return x + y
+
+
+num1 = 1
+num2 = 2
+
+result = add(num1, num2)
+
+print(f"This is the sum: {num1} + {num2}\nand the result is: {result}")
---
+++ test_hello.py
@@ -0,0 +1,7 @@
+from hello import add
+
+def test_add():
+    # make sure when I put 1 and 2 here the output is 3
+    assert add(1, 2) == 3
+    
+    
